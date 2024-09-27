de50006776387a82e49a518b3bacd91bec69ddd1
MartryatusSofia <martryatuss@gmail.com>
2024-09-28 06:55:18 +0700

    praktikum 01

---
+++ 01_Pengenalan_CPP_Bagian_1/Guided/Latihan_05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+    int i = 1;
+    int jum;
+    cin>>jum;
+    do{
+        cout<<"baris ke-"<<i+1<<endl;
+        i++;
+    } while(i<jum);
+    getch();
+    return 0;
+}
